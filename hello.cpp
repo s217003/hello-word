2ae7e8ac24c3483ad94d28717e9bdbc2ecd0b3e8
skx217003 <3200103310@zju.edu.cn>
2022-06-27 11:51:15 +0800

    update a cpp file

---
+++ hello.cpp
@@ -0,0 +1,26 @@
+/**
+ *@file   hello.cpp
+ *@author skx217003 
+ *@date   Sun Sep 19 01:16:39 2021
+ *
+ *@brief  A demo program for studying.
+ *
+ *
+ */
+
+#include<iostream>
+
+/**
+ *main function.
+ *
+ *@param argc number of shell parameters.
+ *@param argv strings for all parameters.
+ *
+ *@return system signals.
+ */
+int main(int argc,char* argv[])
+{
+  ///Just a demo.
+  std::cout << "hello world!" << std::endl;
+  return 0;
+};
